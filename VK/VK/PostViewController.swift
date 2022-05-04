7bffbf0cd999f647ee1bcb5fd666583027f679db
CheshireJobs <olyurinova@icloud.com>
2022-05-04 06:26:51 +0300

    feature/task-6/implementation -  changed "Feed" tab at MVC pattern

--- VK/VK/PostViewController.swift
+++ VK/VK/PostViewController.swift
@@ -4,9 +4,38 @@ import StorageService
 class PostViewController: UIViewController {
     
     var post: Post?
+    private lazy var rightItemBarButton: UIBarButtonItem = {
+        var rightItemBarButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showInfoViewController))
+        return rightItemBarButton
+    }()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
  
     override func viewDidLoad() {
         super.viewDidLoad()
         title = post?.title
+        
+        view.backgroundColor = .white
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationItem.rightBarButtonItem = rightItemBarButton
+    }
+    
+}
+
+extension PostViewController {
+    @objc
+    private func showInfoViewController() {
+        let infoViewController = InfoViewController()
+        infoViewController.modalPresentationStyle = .formSheet
+        self.navigationController?.present(infoViewController, animated: true)
     }
 }
